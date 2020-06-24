abaad4a081bce07560fcfbd677ebfcaef93865ad
LeoNozaki <leosnozaki@gmail.com>
2020-06-23 22:09:30 -0300

    Leitura de multiplas linhas para string de palheiro (ate EOF), exibe o tamanho das strings lidas

--- stringMatch.cpp
+++ stringMatch.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main () {
-  string haystack, needle;
+  string haystack, needle, auxRead;
+
   getline(cin, needle);
 	getline(cin, haystack);
+  while(getline(cin, auxRead)) {
+		haystack = haystack + ' ' + auxRead;
+	};
+
+  int lenHaystack = haystack.length();
+	int lenNeedle = needle.length();
   
-  cout<<needle<<haystack<<endl;
+  cout<<needle<<" "<<haystack<<endl;
+  cout<<lenNeedle<<" "<<lenHaystack<<endl;
   return 0;
 }
