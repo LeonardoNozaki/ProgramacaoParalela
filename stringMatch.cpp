2c5070fd1eac67927b12b70338acd906406dda09
LeoNozaki <leosnozaki@gmail.com>
2020-06-23 21:36:33 -0300

    Codigo inicial, leitura de duas strings e imprime o resultado

---
+++ stringMatch.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  string haystack, needle;
+  getline(cin, needle);
+	getline(cin, haystack);
+  
+  cout<<needle<<haystack<<endl;
+  return 0;
+}
