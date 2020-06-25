6c330a4824c2b5fc4583326b69d3170c2fea1685
LeoNozaki <leosnozaki@gmail.com>
2020-06-24 21:23:13 -0300

    Leitura de dados por arquivo e utilizacao de openmp (pragma)

--- stringMatch.cpp
+++ stringMatch.cpp
@@ -1,15 +1,24 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 #include <chrono>
 using namespace std;
 
 int main () {
   string haystack, needle, auxRead;
 
+  cout<<"Digite o texto de busca: ";
   getline(cin, needle);
-	getline(cin, haystack);
-  while(getline(cin, auxRead)) {
-		haystack = haystack + '\n' + auxRead;
-	};
+  cout<<"Digite o nome do arquivo que contem o texto de entrada: ";
+  getline(cin, auxRead);
+
+  ifstream file(auxRead);
+  if (file.is_open()) {
+    while (!file.eof()) {
+      getline (file, auxRead);
+      haystack = haystack + '\n' + auxRead;
+    }
+    file.close();
+  }
   
   int lenHaystack = haystack.length();
 	int lenNeedle = needle.length();
@@ -18,12 +27,14 @@ int main () {
 
   auto inicio = chrono::high_resolution_clock::now();
 
+  #pragma omp parallel for num_threads(1)
   for(i = 0; i < lenHaystack; i++) { 
 	  int j = 0;
 		while ( j < lenNeedle && (tolower(haystack[i + j]) == tolower(needle[j])) ) {
 			j++;
 		}
 		if (j == lenNeedle) {
+      #pragma omp critical
 			q.push(i);
 		}
 	}
